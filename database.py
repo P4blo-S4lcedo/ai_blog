77398c32cd6872a9d7e7356ddcc9fe694cb15395
P4bloS4lcedo <juanpas2006.jpsp@gmail.com>
2025-11-20 01:21:57 -0500

    Cambiar configuración a PostgreSQL para Render

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ MYSQL_HOST = "localhost"
 MYSQL_PORT = "3306"
 MYSQL_DB = "ai_blog"
 
-DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://ai_blogdb_user:BizqgHMOr7EYb3Xk57wvtmJ1JdFYI8Eb@dpg-d4favlnpm1nc73eqv5tg-a.oregon-postgres.render.com/ai_blogdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
